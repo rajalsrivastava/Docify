3988faeff95376a2c90b6e56fd191d59ae512bdd
Rajal Srivastava <rajal10srivastava@gmail.com>
2025-03-15 15:29:04 +0530

    integrate tiptap editor

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,12 +1,16 @@
-interface DocumentIdPageProps{
-    params:Promise<{documentId:string}>
+import { Editor } from "./editor";
+
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: string }>;
 }
 
-const DocumentIdPage =async ({params}:DocumentIdPageProps) => {
-    const {documentId} =await params;
-    return (
-    <div>Document Id: {documentId}</div>
-  )
-}
+const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Editor />
+    </div>
+  );
+};
 
-export default DocumentIdPage
+export default DocumentIdPage;
