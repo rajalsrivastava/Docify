64aef272befc507f73968339ed922f679b4642b4
Rajal Srivastava <rajal10srivastava@gmail.com>
2025-03-15 16:51:29 +0530

    add toolbar with undo button

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
@@ -10,8 +9,35 @@ import ImageResize from "tiptap-extension-resize-image";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
+import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditorStore } from "@/store/use-editor-store";
 export const Editor = () => {
+  const { setEditor } = useEditorStore();
   const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy(){
+        setEditor(null);
+    },
+    onUpdate({editor}){
+        setEditor(editor);
+    },
+    onSelectionUpdate({editor}){
+        setEditor(editor);
+    },
+    onTransaction({editor}){
+        setEditor(editor);
+    },
+    onFocus({editor}){
+        setEditor(editor);
+    },
+    onBlur({editor}){
+        setEditor(editor);
+    },
+    onContentError({editor}){
+        setEditor(editor);
+    },
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right:56px;",
@@ -52,7 +78,7 @@ export const Editor = () => {
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
-        <EditorContent editor={editor}/>
+        <EditorContent editor={editor} />
       </div>
     </div>
   );
