0b77c0b0e558ea9485020d53e737e03095a823ea
Rajal Srivastava <rajal10srivastava@gmail.com>
2025-03-16 12:49:51 +0530

    add more editor tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -6,6 +6,7 @@ import TaskList from "@tiptap/extension-task-list";
 import Table from "@tiptap/extension-table";
 import Image from "@tiptap/extension-image";
 import ImageResize from "tiptap-extension-resize-image";
+import Underline from "@tiptap/extension-underline";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
@@ -47,6 +48,7 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      Underline,
       Image,
       ImageResize,
       Table,
