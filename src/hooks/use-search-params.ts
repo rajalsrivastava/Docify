4d4c5b4f7de76a40aa9151249a8eab399df8773a
Rajal Srivastava <rajal10srivastava@gmail.com>
2025-03-20 20:19:00 +0530

    add search input component

---
+++ src/hooks/use-search-params.ts
@@ -0,0 +1,5 @@
+import {parseAsString, useQueryState} from "nuqs";
+
+export function useSearchParam() {
+    return useQueryState("search", parseAsString.withDefault("").withOptions({clearOnDefault:true}));
+}
